6f113afef751c53911b4972b9935cf0e34d14440
Audrey Park <ap59462@my.utexas.edu>
2026-04-19 22:13:14 -0500

    Update Globals.cs

--- Globals.cs
+++ Globals.cs
@@ -38,7 +38,7 @@ public class Globals : Game
         GraphicsDevice = base.GraphicsDevice;
         SpriteBatch = new SpriteBatch(GraphicsDevice);
         
-        Atlas = TextureAtlas.FromFile(Content, "Textures/atlas.xml");
+        Atlas = TextureAtlas.FromFile(Content, "atlas.xml");
         
         base.Initialize();
     }
