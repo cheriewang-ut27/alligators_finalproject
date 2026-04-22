a64c4fc023dcbe20ef99cad5d6b1361cc87df7fd
sameerb1234 <Sameermbaig0@gmail.com>
2026-04-21 23:14:17 -0500

    UPDATED game 1
    Removed volume control functionality and related variables.

--- Game1.cs
+++ Game1.cs
@@ -4,13 +4,12 @@ using alligators_finalproject.Content;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Audio;
 
 namespace alligators_finalproject;
 
 public class Game1 : Globals
 {
-
+    //private Jellyfish jellyfish;
     private List<Jellyfish> jellyfishList = new List<Jellyfish>();
     
     private Fish testFish;
@@ -18,23 +17,19 @@ public class Game1 : Globals
     private Texture2D fishTexture2;
     private Texture2D fishTexture3;
 
-    
+    // ALLIGATOR
     private Alligator alligator;
     private Texture2D gator1;
     private Texture2D gator2;
     private Texture2D gator3;
     private Texture2D gator4;
 
-   
+    // GUI part
     private SpriteFont hudFont;
     
     private Tilemap tilemap;
     private GameManager gameManager;
     private HUD hud;
-
-    private float volume = 0.5f;
-    private bool isMuted = false;
-    private KeyboardState previousKeyboardState;
 
     public Game1()  : base(1280, 720, false)
     {
@@ -42,18 +37,24 @@ public class Game1 : Globals
 
     protected override void Initialize()
     {
+        
         base.Initialize();
+        
         gameManager = new GameManager();
+
     }
 
     protected override void LoadContent()
     {
+        
+        
         if (Atlas == null) {
             Atlas = TextureAtlas.FromFile(Content, "textures/atlas.xml");
         }
 
         tilemap = Tilemap.FromFile(Content, "textures/tilemap.xml");
         tilemap.Scale = new Vector2(0.4f, 0.4f);
+        
         
         fishTexture1 = Content.Load<Texture2D>("fish/fish1");
         fishTexture2 = Content.Load<Texture2D>("fish/fish2");
@@ -85,15 +86,14 @@ public class Game1 : Globals
             0.2f
         );
         
+        //jellyfish = new Jellyfish(new Vector2(400, 300), ScreenBounds);
         jellyfishList.Add(new Jellyfish(new Vector2(800, 300), ScreenBounds));
         jellyfishList.Add(new Jellyfish(new Vector2(600, 300), ScreenBounds));
-
-        hudFont = Content.Load<SpriteFont>("HudFont");
-        hud = new HUD(hudFont);
-
-        SoundEffect.MasterVolume = volume;
      
+        hud = new HUD(Content.Load<SpriteFont>("HudFont"));
+        
         base.LoadContent();
+        
     }
 
     protected override void Update(GameTime gameTime)
@@ -102,8 +102,6 @@ public class Game1 : Globals
 
         if (kb.IsKeyDown(Keys.Escape))
             Exit();
-
-        HandleVolumeControls(kb);
 
         switch (gameManager.CurrentState)
         {
@@ -114,11 +112,12 @@ public class Game1 : Globals
                 break;
 
             case GameState.Playing:
+
                 foreach (Jellyfish jellyfish in jellyfishList)
                 {
                     jellyfish.Update(gameTime);
+                    
                 }
-
                 alligator.Update(gameTime);
                 testFish.FleeFrom(alligator.GetPosition());
                 testFish.Update(gameTime);
@@ -132,48 +131,28 @@ public class Game1 : Globals
             case GameState.Lose:
                 if (kb.IsKeyDown(Keys.R))
                 {
+                    // initialize new jellyfish
                     RestartGame();
                 }
+                    
                 break;
         }
 
-        previousKeyboardState = kb;
         base.Update(gameTime);
-    }
-
-    private void HandleVolumeControls(KeyboardState kb)
-    {
-        if (kb.IsKeyDown(Keys.M) && !previousKeyboardState.IsKeyDown(Keys.M))
-        {
-            isMuted = !isMuted;
-            SoundEffect.MasterVolume = isMuted ? 0f : volume;
-        }
-
-        if (kb.IsKeyDown(Keys.OemComma) && !previousKeyboardState.IsKeyDown(Keys.OemComma))
-        {
-            volume = Math.Max(0f, volume - 0.1f);
-            if (!isMuted)
-                SoundEffect.MasterVolume = volume;
-        }
-
-        if (kb.IsKeyDown(Keys.OemPeriod) && !previousKeyboardState.IsKeyDown(Keys.OemPeriod))
-        {
-            volume = Math.Min(1f, volume + 0.1f);
-            if (!isMuted)
-                SoundEffect.MasterVolume = volume;
-        }
     }
 
     private void HandleCollisions()
     {
+
         if (testFish.GetBounds().Intersects(alligator.GetBounds()) &&
             alligator.IsMouthClosed())
         {
             gameManager.AddScore(1);
             testFish.SpawnFish();
+            
         }
-
-    
+        // Jellyfish collision
+        
         foreach (Jellyfish jellyfish in jellyfishList)
         {
             if (alligator.GetBounds().Intersects(jellyfish.BoundingRectangle))
@@ -205,6 +184,7 @@ public class Game1 : Globals
         
         tilemap.Draw(SpriteBatch);
 
+
         if (gameManager.CurrentState != GameState.Menu)
         {
             testFish.Draw(SpriteBatch);
@@ -214,18 +194,10 @@ public class Game1 : Globals
             {
                 jellyfish.Draw(SpriteBatch);
             }
+            
         }
 
         hud.Draw(SpriteBatch, gameManager);
-
-        
-        string volumeText = isMuted ? "Volume: Muted" : $"Volume: {(int)(volume * 100)}%";
-        Vector2 textSize = hudFont.MeasureString(volumeText);
-        Vector2 textPosition = new Vector2(ScreenBounds.Width - textSize.X - 20, 20);
-
-        SpriteBatch.DrawString(hudFont, volumeText, textPosition, Color.White);
-        SpriteBatch.DrawString(hudFont, "M = Mute   , = Down   . = Up", new Vector2(ScreenBounds.Width - 320, 50), Color.White);
-
         SpriteBatch.End();
 
         base.Draw(gameTime);
